24e8232f20fd4d8b9e091c17296ec3e96a0aa8c3
Andrew Kinsman <69199502+LunaPurpleSunshine@users.noreply.github.com>
2023-06-03 00:47:59 +0100

    Use ipaddress module for ip addresses

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import urllib.request as req
 from datetime import datetime, timezone
 from random import randint
+from ipaddress import IPv4Address
 
 import pytest
 from discord_webhook import DiscordWebhook
@@ -43,12 +44,22 @@ def sqlite_in_memory(monkeypatch: MonkeyPatch):
 
 
 @pytest.fixture
-def ip_data_static() -> tuple[datetime, str]:
-    return (datetime(1963, 11, 23, 17, 16), "10.10.10.0")
+def ip_data_static() -> tuple[datetime, IPv4Address]:
+    return (datetime(1963, 11, 23, 17, 16), IPv4Address("10.10.10.0"))
 
+
+
+def generate_random_ipv4_address() -> IPv4Address:
+    """ Returns a completely random ipaddress.IPv4Address
+    In this function, random.randint() is used to generate a random integer 
+    within the valid range of IPv4 addresses (from 0 to 2^32 - 1),
+    the integer is converted to an IPv4Address object using ipaddress.IPv4Address()
+    """
+    ip_int = randint(0, 2**32 - 1)
+    return IPv4Address(ip_int)
 
 @pytest.fixture
-def ip_data_random() -> tuple[datetime, str]:
+def ip_data_random() -> tuple[datetime, IPv4Address]:
     dt = datetime.now(timezone.utc).replace(microsecond=0)
-    ip = ".".join(str(randint(0, 255)) for _ in range(4))
+    ip = generate_random_ipv4_address()
     return dt, ip
